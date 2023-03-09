81b8b63907dcece82f303d4c48d9200b3b415bfa
james <james@devpipeline.com>
2023-03-09 11:48:58 -0700

    first commit, testing math.floor

---
+++ script.js
@@ -0,0 +1,15 @@
+/* 
+Challenge: 
+Write a program that will return a random hexadecimal value. 
+Hexadecimal values => # -> a-f -> 0-9
+randomHex => #aaa670
+randomHex => #be7380
+*/
+
+function randomHexGenerator() {
+  const hexCharacters = "ABCDEF1234567890"
+}
+
+stri = "ab"
+console.log(stri.length)
+console.log(stri[Math.floor(Math.random() * stri.length)])
